785b20b598b4963fd1f7f90b26d7e9392727e413
murashko12 <pert@ocumare.ru>
2023-08-20 16:48:22 +0300

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Sidenav from './components/Sidenav'
-import Navbar from './components/Navbar'
+
+import MainPage from './pages/MainPage'
+import AboutPage from './pages/AboutPage'
+import Layout from './components/Layout/Layout'
 
 function App() {
 
-  return (
-    <>
-      <Navbar/>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path={'/'} element={<Layout/>}>
+                    <Route path={'/'} element={<MainPage/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                </Route>
+            </Routes>
+        </>
+    )
 }
 
 export default App
---
+++ src/pages/CoursesPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CoursesPage = () => {
+  return (
+    <div>
+      courses
+    </div>
+  )
+}
+
+export default CoursesPage
