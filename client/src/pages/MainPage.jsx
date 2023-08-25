3dca3eb9a69ab64b976ddd759985fbcde44aa1cb
murashko12 <pert@ocumare.ru>
2023-08-25 12:04:06 +0300

    main page

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,19 +1,27 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Divider, Typography } from '@mui/material'
 import React from 'react'
 
 import mpeiLogo from '../Images/mpeiLogo.png'
 
 const MainPage = () => {
     return (
-        <Box sx={{width: "100%", height: "auto"}}>
-            <Typography variant="h1" gutterBottom fontWeight={400} color={"#adadad"}>
-                IT - клуб при НИУ МЭИ
-            </Typography>
-            <Typography variant="h1" gutterBottom fontWeight={400} color={"#adadad"}>
-                TURING
-            </Typography>
-            <img height={"250px"} src={mpeiLogo} alt="" />
-        </Box>  
+        <>
+            <Box sx={{width: "100%", height: "auto"}}>
+                <Typography variant="h1" gutterBottom fontWeight={400} color={"#adadad"} textAlign={"center"}>
+                    IT - клуб при НИУ МЭИ
+                </Typography>
+                <Typography variant="h1" gutterBottom fontWeight={400} color={"#006aff"} textAlign={"center"}>
+                    TURING
+                </Typography>
+                <Box sx={{width: "100%",display: "flex", justifyContent: "center"}}>
+                    <img height={"250px"} src={mpeiLogo} alt="" />
+                </Box>
+                <Divider sx={{borderBottomWidth: 3,margin: "50px 0px"}} />
+                <Typography variant="h3" gutterBottom fontWeight={400} color={"#006aff"} textAlign={"center"}>
+                    TURING
+                </Typography>
+            </Box>  
+        </>
     )
 }
 
